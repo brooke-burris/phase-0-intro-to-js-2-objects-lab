ddda2b76ded82853ba788c3db3e7d1a1327072d8
Brooke <brooke-burris@outlook.com>
2023-01-18 21:41:14 -0600

    commit tree 5bbb3bbc1d5c5fefb142582bb4c97eb7c3379e55

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+const employees = {
+    employee:
+    { name: "pumpkin",
+    streetAddress: "3rd tower",
+}
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = updateEmployeeWithKeyAndValue(
+        employee, "name", "Sam" + "streetAddress", "11 Broadway"
+    )
+}
